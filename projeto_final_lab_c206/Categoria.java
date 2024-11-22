4dce6900b270cfca8e1a080eb8eb38f213c45152
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2024-11-22 11:32:33 -0300

    Implementação da classe Categoria

---
+++ projeto_final_lab_c206/Categoria.java
@@ -0,0 +1,20 @@
+package projeto_final_lab_c206;
+
+public class Categoria extends Livro {
+    private String categoria;
+
+    public Categoria(String ID, String nome, String autor, String categoria) {
+        super(ID, nome, autor);
+        this.categoria = categoria;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Digite a categoria do livro: " + categoria;
+    }
+    
+}
