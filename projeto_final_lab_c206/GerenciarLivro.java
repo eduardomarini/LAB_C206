d3b9010e871d83ac3d481f2ab80096f6e3931dd1
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2024-11-22 11:20:08 -0300

    Implementação dos metodos salvarDados / carregarDados

--- projeto_final_lab_c206/GerenciarLivro.java
+++ projeto_final_lab_c206/GerenciarLivro.java
@@ -1,13 +1,20 @@
 package projeto_final_lab_c206;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class GerenciarLivro {
     
     private final ArrayList<Livro> livros = new ArrayList<>();
+    private static final String NOME_ARQUIVO = "livros_cadastrados.txt";
+
+    public GerenciarLivro() {
+        carregarDados();
+    }
 
     public void addLivro(Livro livro) {
         livros.add(livro);
+        salvarDados();
     }
 
     public Livro procurarPorID(String ID) {
@@ -45,6 +52,36 @@ public class GerenciarLivro {
             for (Livro livro : livros) {
                 System.out.println(livro);
             }
+        }
+    }
+
+    private void salvarDados() {
+        // Implementar a lógica para salvar os livros no arquivo
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(NOME_ARQUIVO))) {
+            for (Livro livro : livros) {
+                writer.write("ID: " + livro.getID() + ", " +"Nome do livro: " + livro.getNome() + ", " + "Nome do Autor: " + livro.getAutor());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar os dados no arquivo." + e.getMessage());
+        }
+    }
+
+    private void carregarDados() {
+        // Implementar a lógica para carregar os livros do arquivo
+        try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
+            String linha;
+            while ((linha = reader.readLine()) != null) {
+                String[] dados = linha.split(",");
+                if (dados.length != 3) {
+                    Livro livro = new Livro(dados[0], dados[1], dados[2]);
+                    livros.add(livro);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Erro ao carregar os dados do arquivo." + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Erro ao fechar o arquivo." + e.getMessage());
         }
     }
 }
