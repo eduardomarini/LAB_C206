7f593d3a1ef1bf16bf2c3f4c32f9f9960576d66a
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2024-11-22 15:00:58 -0300

    formatação do .txt, adição do parâmetro novaCAtegoria

--- projeto_final_lab_c206/GerenciarLivro.java
+++ projeto_final_lab_c206/GerenciarLivro.java
@@ -26,11 +26,15 @@ public class GerenciarLivro {
         return null;
     }
 
-    public boolean atualizarLivro(String ID, String novoNome, String novoAutor) {
+    public boolean atualizarLivro(String ID, String novoNome, String novoAutor, String novaCategoria) {
         Livro livro = procurarPorID(ID);
         if (livro != null) {
             livro.setNome(novoNome);
             livro.setAutor(novoAutor);
+            if (livro instanceof Categoria) {
+                ((Categoria) livro).setCategoria(novaCategoria);
+            }
+            salvarDados();
             return true;
         }
         return false;
@@ -40,6 +44,7 @@ public class GerenciarLivro {
         Livro livro = procurarPorID(ID);
         if (livro != null) {
             livros.remove(livro);
+            salvarDados();
             return true;
         }
         return false;
@@ -56,32 +61,40 @@ public class GerenciarLivro {
     }
 
     private void salvarDados() {
-        // Implementar a lógica para salvar os livros no arquivo
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(NOME_ARQUIVO))) {
             for (Livro livro : livros) {
-                writer.write("ID: " + livro.getID() + ", " +"Nome do livro: " + livro.getNome() + ", " + "Nome do Autor: " + livro.getAutor());
+                if (livro instanceof Categoria) {
+                    Categoria categoria = (Categoria) livro;
+                    writer.write("ID: " + categoria.getID() + ", Nome do  livro: " + categoria.getNome() + ", Nome do Autor: " + categoria.getAutor() + ", Categoria: " + categoria.getCategoria());
+                } else {
+                    writer.write(livro.getID() + "," + livro.getNome() + "," + livro.getAutor());
+                }
                 writer.newLine();
             }
+            System.out.println("Dados salvos com sucesso.");
         } catch (IOException e) {
-            System.out.println("Erro ao salvar os dados no arquivo." + e.getMessage());
+            System.out.println("Erro ao salvar os dados no arquivo: " + e.getMessage());
         }
     }
 
     private void carregarDados() {
-        // Implementar a lógica para carregar os livros do arquivo
         try (BufferedReader reader = new BufferedReader(new FileReader(NOME_ARQUIVO))) {
             String linha;
             while ((linha = reader.readLine()) != null) {
                 String[] dados = linha.split(",");
-                if (dados.length != 3) {
+                if (dados.length == 4) {
+                    Categoria categoria = new Categoria(dados[0], dados[1], dados[2], dados[3]);
+                    livros.add(categoria);
+                } else if (dados.length == 3) {
                     Livro livro = new Livro(dados[0], dados[1], dados[2]);
                     livros.add(livro);
                 }
             }
+            System.out.println("Dados carregados com sucesso.");
         } catch (FileNotFoundException e) {
-            System.out.println("Erro ao carregar os dados do arquivo." + e.getMessage());
+            System.out.println("Arquivo de dados não encontrado. Um novo arquivo será criado.");
         } catch (IOException e) {
-            System.out.println("Erro ao fechar o arquivo." + e.getMessage());
+            System.out.println("Erro ao carregar os dados do arquivo: " + e.getMessage());
         }
     }
 }
